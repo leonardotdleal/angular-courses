016f084752922c5dc20393f856be649eafb04e11
Leonardo Tadeu Diniz Leal <leonardo.leal@jv01.local>
2019-04-04 08:24:54 -0300

    Module 3 class adding a site header

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,15 +1,18 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
